8efdf3c7fe4861471d4a95e1e855697a3cc0f794
Tony Sun <tsun@zendesk.com>
2022-02-12 15:51:32 +1100

    feat: add axios client to post to pinata

---
+++ minter-starter-files/src/utils/pinata.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const key = process.env.REACT_APP_PINATA_API_KEY;
+const secret = process.env.REACT_APP_PINATA_API_SECRET;
+
+const axios = require('axios');
+
+export const pinJSONtoIPFS = async (body) => {
+  const url = 'https://api.pinata.cloud/pinning/pinJSONToIPFS'
+  return axios.post(url, body, {
+    headers: {
+      pinata_api_key: key,
+      pinata_secret_api_key: secret,
+    }
+  }).then(response => {
+    return {
+      success: true,
+      // this Url will be used as the tokenURI input into the mint function of the contract.
+      pinataUrl: 'https://gateway.pinata.cloud/ipfs/' + response.data.IpfsHash,
+    }
+  }).catch(error => {
+    console.log(error)
+    return {
+      success: false,
+      message: error.message
+    }
+  })
+}
